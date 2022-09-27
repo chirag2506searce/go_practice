c4e79cd01c312bc6e6fe0f49557c90f7ea45e49a
chirag2506 <chiraggupta2506@gmail.com>
2022-09-27 10:04:02 +0530

    channels

--- go_udemy/channels/main.go
+++ go_udemy/channels/main.go
@@ -1,66 +1,60 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"time"
-// )
-
-// func main() {
-// 	links := []string{
-// 		"http://golang.org",
-// 		"http://google.com",
-// 		"http://facebook.com",
-// 		"http://stackoverflow.com",
-// 		"http://amazon.com",
-// 		"http://flipkart.com",
-// 		"http://apple.com",
-// 		"http://netflix.com",
-// 	}
-// 	// fmt.Println(links)
-// 	c := make(chan string)
-// 	for _, link := range links {
-// 		go checkLink(link, c)
-// 	}
-
-// 	// fmt.Println(<-c)
-// 	// fmt.Println(<-c)
-// 	// fmt.Println(<-c)
-// 	// do above or {
-// 	// for i := 0; i < len(links); i++ {
-// 	// 	fmt.Println(<-c)
-// 	// }
-// 	// }
-// 	for l := range c {
-// 		// for {
-// 		// go checkLink(<-c, c)
-
-// 		// go checkLink(l, c)
-// 		go func(link string) {
-// 			time.Sleep(5 * time.Second)
-// 			checkLink(link, c)
-// 		}(l)
-
-// 	}
-// 	// fmt.Println(<-c) // will hang, because len(links) = 8
-
-// }
-
-//	func checkLink(link string, c chan string) {
-//		_, err := http.Get(link)
-//		if err != nil {
-//			fmt.Println(link, "might be down")
-//			// c <- "Its down"
-//			c <- link
-//			return
-//		}
-//		fmt.Println(link, "is up")
-//		// c <- "Its up"
-//		c <- link
-//	}
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
 func main() {
+	links := []string{
+		"http://golang.org",
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://amazon.com",
+		"http://flipkart.com",
+		"http://apple.com",
+		"http://netflix.com",
+	}
+	// fmt.Println(links)
 	c := make(chan string)
-	c <- "Hi there!"
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// do above or {
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+	// }
+	for l := range c {
+		// for {
+		// go checkLink(<-c, c)
+
+		// go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+
+	}
+	// fmt.Println(<-c) // will hang, because len(links) = 8
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		// c <- "Its down"
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up")
+	// c <- "Its up"
+	c <- link
 }
